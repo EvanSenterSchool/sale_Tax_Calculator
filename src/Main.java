20b6ae4443b8e08455827055068ce5286b4d474d
Evan Senter (614925) <614925@swl.local>
2024-09-12 08:41:05 -0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double productPrice;
+        double salesTax = 0.05;
+        double finalPrice;
+
+        System.out.println("What is the price of your item");
+        Scanner scan = new Scanner(System.in);
+        productPrice = Double.parseDouble(scan.nextLine());
+
+        finalPrice = productPrice + (productPrice * salesTax);
+        System.out.println("Your product with tax is $" + finalPrice);
     }
+
 }
